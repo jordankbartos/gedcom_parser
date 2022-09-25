202b3b728720efb55a79f06b298b232bbcae6ec7
Jordan Bartos <jordankbartos@gmail.com>
2022-09-25 14:16:28 -0700

    Deployed a8a5541 with MkDocs version: 1.3.1

--- gen_ref_pages.py
+++ gen_ref_pages.py
@@ -28,14 +28,17 @@ for path in sorted(Path("gedcom_parser").rglob("*.py")):
     elif parts[-1] == "__main__":
         continue
 
-    nav[parts] = doc_path.as_posix()
+    if parts:
+        nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        print("writing")
-        ident = ".".join(parts)
-        fd.write(f"::: {ident}")
+        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+            print("writing")
+            ident = ".".join(parts)
+            fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+        mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
+    for l in nav.build_literate_nav():
+        print(l)
